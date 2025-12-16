4cc24f04c2a7119af001fec5f8c0af5a1e588af2
Ahmed H Mekky <ahmedusseinmekky1@gmail.com>
2025-12-16 19:51:43 +0200

    Third Amendment

--- lib/features/home/presentation/pages/home/home_page.dart
+++ lib/features/home/presentation/pages/home/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:cure_team_2/features/home/presentation/pages/favorite/presentation/pages/favorite_page.dart';
+import 'package:cure_team_2/features/home/presentation/pages/favorite/pages/favorite_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
