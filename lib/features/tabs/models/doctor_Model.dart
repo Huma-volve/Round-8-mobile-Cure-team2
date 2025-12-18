580aaeca827658e2fde2d713cb6e67b453cdc171
saaadgawesh <saadgawesh608@gmail.com>
2025-12-18 14:01:59 +0200

    completeMybookingScreen

--- lib/features/tabs/models/doctor_Model.dart
+++ lib/features/tabs/models/doctor_Model.dart
@@ -1,48 +1,43 @@
-class BookingModel {
-  final String doctorName;
-  final String specialty;
-  final String address;
-  final DateTime dateTime;
-  final BookingStatus status;
-  final String image;
+// import 'package:flutter/material.dart';
 
-  BookingModel({
-    required this.doctorName,
-    required this.specialty,
-    required this.address,
-    required this.dateTime,
-    required this.status,
-    required this.image,
-  });
-}
-List<BookingModel> bookings = [
-  BookingModel(
-    doctorName: 'Jennifer Miller',
-    specialty: 'Psychiatrist',
-    address: '129, El-Nasr Street, Cairo, Egypt',
-    dateTime: DateTime(2025, 7, 21, 11, 0),
-    status: BookingStatus.upcoming,
-    image: 'assets/images/doctor.png',
-  ),
-  BookingModel(
-    doctorName: 'Jennifer Miller',
-    specialty: 'Psychiatrist',
-    address: '129, El-Nasr Street, Cairo, Egypt',
-    dateTime: DateTime(2025, 7, 21, 11, 0),
-    status: BookingStatus.completed,
-    image: 'assets/images/doctor.png',
-  ),
-  BookingModel(
-    doctorName: 'Jennifer Miller',
-    specialty: 'Psychiatrist',
-    address: '129, El-Nasr Street, Cairo, Egypt',
-    dateTime: DateTime(2025, 7, 21, 11, 0),
-    status: BookingStatus.canceled,
-    image: 'assets/images/doctor.png',
-  ),
-];
-enum BookingStatus {
-  upcoming,
-  completed,
-  canceled,
-}
+// class BookingModel {
+//   final String doctorName;
+//   final String specialty;
+//   final String address;
+//   final DateTime dateTime;
+//   final BookingStatus status;
+
+//   BookingModel({
+//     required this.doctorName,
+//     required this.specialty,
+//     required this.address,
+//     required this.dateTime,
+//     required this.status,
+//   });
+// }
+
+// List<BookingModel> bookings = [
+//   BookingModel(
+//     doctorName: 'Jennifer Miller',
+//     specialty: 'Psychiatrist',
+//     address: '129, El-Nasr Street, Cairo, Egypt',
+//     dateTime: DateTime(2025, 7, 21, 11, 0),
+//     status: BookingStatus.upcoming,
+//   ),
+//   BookingModel(
+//     doctorName: 'Jennifer Miller',
+//     specialty: 'Psychiatrist',
+//     address: '129, El-Nasr Street, Cairo, Egypt',
+//     dateTime: DateTime(2025, 7, 21, 11, 0),
+//     status: BookingStatus.completed,
+//   ),
+//   BookingModel(
+//     doctorName: 'Jennifer Miller',
+//     specialty: 'Psychiatrist',
+//     address: '129, El-Nasr Street, Cairo, Egypt',
+//     dateTime: DateTime(2025, 7, 21, 11, 0),
+//     status: BookingStatus.canceled,
+//   ),
+// ];
+
+
--- lib/features/tabs/widgets/CustomOutlinedButton.dart
+++ lib/features/tabs/widgets/CustomOutlinedButton.dart
@@ -1,25 +1,31 @@
+import 'package:cure_team_2/core/theme/app_colors.dart';
 import 'package:cure_team_2/core/theme/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
-class custombuttonItem extends StatelessWidget {
-  const custombuttonItem({
+class CustomOutlinedButton extends StatelessWidget {
+  const CustomOutlinedButton({
     super.key,
     required this.text,
     required this.onpressed,
+
+    required this.isSelected,
   });
 
-  final String text;
+  final String? text;
+
   final VoidCallback onpressed;
+  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding:const EdgeInsets.all(8),
-      width: 170,
+      padding: const EdgeInsets.all(8),
+      width: 165,
       height: 40,
       decoration: BoxDecoration(
+        color: isSelected ? AppColors.primary : Colors.white,
         borderRadius: BorderRadius.circular(10),
-        border: Border.all(width: 1),
+        border: Border.all(width: 1, color: AppColors.grey),
       ),
       child: OutlinedButton(
         style: OutlinedButton.styleFrom(
@@ -27,7 +33,12 @@ class custombuttonItem extends StatelessWidget {
           side: BorderSide.none,
         ),
         onPressed: onpressed,
-        child: Text(text, style: AppTextStyles.montserratCaption.copyWith()),
+        child: Text(
+          text!,
+          style: AppTextStyles.montserratCaption.copyWith(
+            color: isSelected ? Colors.white : AppColors.grey,
+          ),
+        ),
       ),
     );
   }
