e3902b80744c6a0facb32d5096add53801f3d04e
saaadgawesh <saadgawesh608@gmail.com>
2025-12-21 21:38:24 +0200

    commit

---
+++ lib/Location/LocationScreen.dart
@@ -0,0 +1,72 @@
+import 'package:cure_team_2/Location/Location_Manager.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Locationscreen extends StatefulWidget {
+  const Locationscreen({super.key});
+
+  @override
+  State<Locationscreen> createState() => _LocationscreenState();
+}
+
+class _LocationscreenState extends State<Locationscreen> {
+  ///1.mylocation 2.latlng 3.marker 4.googleMapController
+  final LocationManager mylocation = LocationManager();
+
+  final initialCameraPosition = const CameraPosition(
+    target: LatLng(0, 0),
+    zoom: 14,
+  );
+  Set<Marker> markers = const <Marker>{};
+  GoogleMapController? googleMapController;
+  Set<Polyline> polylines = const <Polyline>{};
+
+  @override
+  void initState() {
+    _getUserLocation();
+    super.initState();
+  }
+
+  Future<void> _getUserLocation() async {
+    final Locationdata = await mylocation.getUserLocation();
+    if (Locationdata == null) return;
+    final currenlocation = LatLng(
+      Locationdata.latitude!,
+      Locationdata.longitude!,
+    );
+    final userMarker = Marker(
+      markerId: const MarkerId('user_location'),
+      position: currenlocation,
+    );
+    googleMapController?.animateCamera(CameraUpdate.newLatLng(currenlocation));
+    setState(() {
+      markers.clear();
+      markers.add(userMarker);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            _getUserLocation();
+          },
+          icon:const Text('getLocation'),
+        ),
+      ),
+      body: GoogleMap(
+        myLocationEnabled: true,
+        myLocationButtonEnabled: true,
+        polylines: polylines,
+        onMapCreated: (controller) {
+          googleMapController = controller;
+        },
+        markers: markers,
+        initialCameraPosition: initialCameraPosition,
+        mapType: MapType.normal,
+      ),
+    );
+  }
+}
--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:cure_team_2/Location/LocationScreen.dart';
 import 'package:cure_team_2/core/routing/app_router.dart';
 import 'package:cure_team_2/core/theme/app_theme.dart';
 import 'package:cure_team_2/features/Favourite/Screens/Your_Favourite.dart';
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         left: false,
         child: MaterialApp(
           title: 'cure_team_1',
-          home: YourFavourite(),
+          home: Locationscreen(),
           // initialRoute:Routes.navbar,
           onGenerateRoute: router.generateRoute,
           theme: AppTheme.lightTheme,
